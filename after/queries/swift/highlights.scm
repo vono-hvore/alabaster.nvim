bccfca5c49d048cdb11a995823932b278a511603
Rodion Hladchenko <rodie.www7801@gmail.com>
2026-01-30 14:45:12 +0200

    save

--- after/queries/swift/highlights.scm
+++ after/queries/swift/highlights.scm
@@ -11,12 +11,6 @@
 (protocol_declaration
   name: (type_identifier) @AlabasterDefinition)
 
-(typealias_declaration
-  name: (type_identifier) @AlabasterDefinition)
-
-;; Type references (when types/classes are used)
-(type_identifier) @type
-
 ;; Literals
 (line_str_text) @AlabasterString
 (multi_line_str_text) @AlabasterString
