c8ed8cf0a3ca15dcce0f97c6983c7e4d0c8499ce
Rodion Hladchenko <rodie.www7801@gmail.com>
2026-01-30 14:41:02 +0200

    save

--- after/queries/swift/highlights.scm
+++ after/queries/swift/highlights.scm
@@ -8,12 +8,6 @@
 (class_declaration
   name: (type_identifier) @AlabasterDefinition)
 
-(struct_declaration
-  name: (type_identifier) @AlabasterDefinition)
-
-(enum_declaration
-  name: (type_identifier) @AlabasterDefinition)
-
 (protocol_declaration
   name: (type_identifier) @AlabasterDefinition)
 
@@ -21,8 +15,7 @@
   name: (type_identifier) @AlabasterDefinition)
 
 ;; Type references (when types/classes are used)
-(user_type
-  (type_identifier) @type)
+(type_identifier) @type
 
 ;; Literals
 (line_str_text) @AlabasterString
