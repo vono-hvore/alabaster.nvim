dcd180d606b55a5dc0b01e9746406f860ec122a9
Rodion Hladchenko <rodie.www7801@gmail.com>
2026-01-30 13:47:35 +0200

    feature: add swift support

--- after/queries/swift/highlights.scm
+++ after/queries/swift/highlights.scm
@@ -5,23 +5,13 @@
 (function_declaration
   (simple_identifier) @AlabasterDefinition)
 
-(protocol_function_declaration
-  name: (simple_identifier) @AlabasterDefinition)
-
-(init_declaration
-  "init" @AlabasterDefinition)
-
 ;; Literals
 (line_str_text) @AlabasterString
 (multi_line_str_text) @AlabasterString
 (raw_str_part) @AlabasterString
-(raw_str_end_part) @AlabasterString
 
 (boolean_literal) @AlabasterConstant
 (integer_literal) @AlabasterConstant
-(hex_literal) @AlabasterConstant
-(oct_literal) @AlabasterConstant
-(bin_literal) @AlabasterConstant
 (real_literal) @AlabasterConstant
 
 ;; Comments
